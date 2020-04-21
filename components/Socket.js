f2ef40f4e0b20cac428f6e819d76579b65ef3da1
Mariana <marianita_hernandez13@hotmail.com>
2020-04-20 21:49:55 -0500

    Socket modificado

--- components/Socket.js
+++ components/Socket.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Button, TextInput } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 
 function UselessTextInput(props) {
   return (
@@ -22,8 +22,8 @@ export default class Socket extends Component {
       txtMensajes: '',
     };
 
-    //this.ws = new WebSocket('ws://192.168.0.105:8080/webSocket/chat');
-    this.ws = new WebSocket('ws://localhost:8080/webSocket/chat');
+    this.ws = new WebSocket('ws://192.168.0.106:8080/webSocket/chat');
+    //this.ws = new WebSocket('ws://localhost:8080/webSocket/chat');
     this.emit = this.emit.bind(this);
 
     console.log('Constructor Called');
@@ -37,7 +37,7 @@ export default class Socket extends Component {
   }
   xd = () => {
     this.setState({
-      usr: this.props.usr,
+      usr: this.props.navigation.getParam("username"),
     });
   };
   componentDidMount() {
@@ -61,16 +61,18 @@ export default class Socket extends Component {
   render() {
     return (
       <View style={{ margin: 10, paddingTop: 30 }}>
-      <Text style={{ fontSize: 20, fontWeight: "bold" }}>
-          Welcome, {this.props.navigation.getParam("username")}
-        </Text>
-        <Button
-          title="Sign out"
-          onPress={() => this.props.navigation.navigate("Login")}
-        />
-        <View> 
-          <Button style={{marginTop: 20}}
-          onPress={() => this.ws.open()}
+        <View>
+          <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+            Welcome, {this.props.navigation.getParam("username")}
+          </Text>
+          <Button
+            title="Sign out"
+            onPress={() => this.props.navigation.navigate("Login")}
+          />
+        </View>
+        <View > 
+          <Button
+          onPress={() => this.ws.onopen()}
           title= 'conectar'
           color = '#24E612'
           />
@@ -84,13 +86,13 @@ export default class Socket extends Component {
           <TextInput
             multiline
             numberOfLines={10}
-            onChangeText={txtMensajes => this.setState({ txtMensajes })}
+            onChangeText={txtMensajes => this.props.navigation.getParam("username")({ txtMensajes })}
             value={this.state.txtMensajes}
             editable={false} 
           />
         </View>
         <TextInput
-          style={{ height: 30, marginTop: 10 }}
+          style={{ height: 40, marginTop: 10, backgroundColor: "#eeeeee", borderRadius: 15 }}
           placeholder="Escribe un mensaje"
           onChangeText={text => this.setState({ text })}
           defaultValue={this.state.text}
@@ -101,7 +103,8 @@ export default class Socket extends Component {
           color="#1480FF"
         />
       </View>
+      
     );
   }
 }
-
+ 
